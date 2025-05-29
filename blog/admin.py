bc67fc10049da261ee6155279f60f7fb53391ba8
eto <aleix.murt22@lacetania.cat>
2025-05-29 18:49:51 +0200

    Update admin.py
    Docstring Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
+"""
+Configuració del panell d'administració de Django per al model Post, Author i Tag.
+
+Aquest mòdul registra els models principals del blog al panell d'administració de Django,
+per tal que puguin ser gestionats fàcilment a través de la interfície administrativa web.
+"""
+
 from django.contrib import admin
 from .models import Post, Author, Tag
 
